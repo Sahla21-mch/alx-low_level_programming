c4848898d752d18d175b002f6407e277d35eecf3
Sahla21-mch <you@example.com>
2022-09-12 11:30:09 +0100

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                               
+                                                                                                                                 
+/**                                                                                                                              
+ * main - Printing                                                                                                               
+ *                                                                                                                               
+ *Return: Always 0 (Successful)                                                                                                  
+ */                                                                                                                              
+int main(void)                                                                                                                   
+{                                                                                                                                
+        puts("\"Programming is like building a multilingual                                                                      
+                        puzzle");                                                                                                
+        return (0);                                                                                                              
 
-/**
-* main - Printing
-*
-* Return: Always 0 (Successful)
-*/
-int main(void)
-{
-    puts("\"Programming is like building a multilingual
-puzzle");
-    return (0);
-}
+A
+A
+A
+
