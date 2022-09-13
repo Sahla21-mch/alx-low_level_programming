4d22947e8036e4341a09f6c3601980a94748129d
Sahla21-mch <you@example.com>
2022-09-13 12:08:40 +0100

    fixed the error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,25 +5,14 @@
 */
 int main(void)
 {
-	_putchar (" ")
-	_putchar ("p")
-	_putchar ("u")
-	_putchar ("t")
-	_putchar ("c")
-	_putchar ("h")
-	_putchar ("a")
-	_putchar ("r")
-	_putchar ("\n")
+	_putchar ('_')
+	_putchar ('p')
+	_putchar ('u')
+	_putchar ('t')
+	_putchar ('c')
+	_putchar ('h')
+	_putchar ('a')
+	_putchar ('r')
+	_putchar ('\n')
 	
 	return (0);
-/**
- * _putchar - writes the character c to stdout
- * @c: the character to print 
- *
- * Return: on success 1.
- * on error, -1 is returned, and errno is set appropriately.
- */
- int _putchar(char c)
- {
- 	return (write(1, &c, 1));
- }  
