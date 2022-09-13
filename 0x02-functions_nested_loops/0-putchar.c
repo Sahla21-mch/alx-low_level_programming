9cf17730823193decbfb8844988e2f309fb3c03d
Sahla21-mch <you@example.com>
2022-09-13 12:46:14 +0100

    p

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,15 @@
 */
 int main(void)
 {
-	putchar ('_');
-	putchar ('p');
-	putchar ('u');
-	putchar ('t');
-	putchar ('c');
-	putchar ('h');
-	putchar ('a');
-	putchar ('r');
-	putchar ('\n');
+	_Ptchar ('_');
+	_Putchar ('p');
+	_Putchar ('u');
+	_Ptchar ('t');
+	_Putchar ('c');
+	_Putchar ('h');
+	_putchar ('a');
+	_Putchar ('r');
+	_Putchar ('\n');
 	
 	return (0);
 }
